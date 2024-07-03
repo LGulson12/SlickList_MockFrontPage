2ccde6460072aca2b66ff090612facdcd1bbd58d
LGulson12 <lukegulson@gmail.com>
2024-07-03 12:09:34 -0500

    Added Google Maps link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,20 @@ const Footer = () => {
       <div className="footer-left">
         <p><FaEnvelope className="icon" /> <a href="mailto:info@medsupply.com">team@slickdent.com</a></p>
         <p><FaPhone className="icon" /> <a href="tel:+12248043892">+1 (773) 230-3009</a></p>
+        <p>
+          <a
+            href="https://www.google.com/maps?q=500+N+Western+Ave,+Lake+Forest,+IL+60045"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>500 N Western Ave, Suite 215</p>
+            <p>Lake Forest, IL 60045</p>
+          </a>
+        </p>
+        <p>
+        </p>
       </div>
+
       <div className="footer-right">
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
